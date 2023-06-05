3966262f61d72c220e0dc960f2d22167003a0891
SARAH <85683667+sarah-albassam@users.noreply.github.com>
2023-06-05 23:32:20 +0300

    Add files via upload

---
+++ GiftsForAll/Csrf-vulnerable/assets/footer.php
@@ -0,0 +1,55 @@
+<!-- ################ Footer ################ -->
+<footer>
+        <div class="inner">
+            <div class="container">
+                <div class="row">
+                   
+                   <div class="col-md-6 foot-about">
+                        <h4>About US</h4>
+
+                        <p>
+                            Gifts For All is a small but motivated company specializing in all kinds of gifts.
+
+                            If you are looking for something new, you are in the right place. We strive to be industrious and innovative, offering our customers something they want, 
+                            putting their desires at the top of our priority list.
+                        </p>
+
+                        <ul>
+                            <li>Khobar, Olaya</li>
+                            <li>sales@GiftsForAll.com</li>
+                            <li>+966 055 1600 085</li>
+                        </ul>
+                    </div>
+                    
+
+                    <div class="col-md-3 foot-services">
+
+                    
+                        <h4>Categories</h4>
+
+                        <ul>
+                            <li><a href="flowers.php">Flowers</a></li>
+                            <li><a href="chocolates.php">Chocolates and Cookies</a></li>
+                            <li><a href="home.php">Home and Living</a></li>
+                            <li><a href="kids.php">Kids</a></li>
+                            
+                           
+                        </ul>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </div>
+       </body>
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+    <script src="assets/plugins/testimonial/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+
+    </html>
+    </footer>
+ 
+
